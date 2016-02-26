c20d11fd2f174320f77eac69de7a7635cdd023cc
Tailin Lo <tl1720@energon1.cims.nyu.edu>
2016-02-26 16:10:26 -0500

    second commit

---
+++ badssc.sh
@@ -0,0 +1,4 @@
+
+# unsuccessful case script
+./Pr1TailinLo.run --sort=Program ../samples/ExtraElses.badssc
+./Pr1TailinLo.run --sort=Program ../samples/Comments.badssc
---
+++ ssc.sh
@@ -0,0 +1,11 @@
+
+# successful case script
+./Pr1TailinLo.run --sort=Program ../samples/Cartesian.ssc
+./Pr1TailinLo.run --sort=Program ../samples/FastFib.ssc
+./Pr1TailinLo.run --sort=Program ../samples/Fib.ssc
+./Pr1TailinLo.run --sort=Program ../samples/HelloJane.ssc
+./Pr1TailinLo.run --sort=Program ../samples/Hello.ssc
+./Pr1TailinLo.run --sort=Program ../samples/Sort.ssc
+./Pr1TailinLo.run --sort=Program ../samples/String.ssc
+./Pr1TailinLo.run --sort=Program ../samples/Comment.ssc
+
